6fa5d32890dc91838fe8a43f4b5704ad9ceebcb8
LukaGitH <luka.kurincic@gmail.com>
2025-03-28 22:15:05 +0100

    Add files via upload

---
+++ ADT74x0.ino
@@ -0,0 +1,15 @@
+#include "ADT74x0.h"
+ADT74x0 adt;
+#define BAUD_RATE 115200
+
+void setup(void)
+{
+  Serial.begin(BAUD_RATE);
+  adt.begin();
+}
+
+void loop(void)
+{
+  Serial.println(adt.readTemperature());
+  delay(100);
+}
